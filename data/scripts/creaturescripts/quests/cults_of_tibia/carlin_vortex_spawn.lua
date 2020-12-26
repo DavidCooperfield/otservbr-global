19438496537d34f588fc4c3f85c6d47f05e5b4ff
Erick Nunes <erickzenho@live.com>
2020-12-26 12:32:17 -0300

    Update carlin_vortex_spawn.lua

--- data/scripts/creaturescripts/quests/cults_of_tibia/carlin_vortex_spawn.lua
+++ data/scripts/creaturescripts/quests/cults_of_tibia/carlin_vortex_spawn.lua
@@ -15,13 +15,13 @@ function vortexCarlin.onKill(creature, target, item)
 			Game.createItem(26140, 1, corpsePosition):setActionId(5580)
 			addEvent(function()
 				local teleport1 = Tile(corpsePosition):getItemById(26140)
-			teleport1:remove(1) end, (1*60*1000), 26140, 1, corpsePosition)
+			teleport1:remove(1) end, (1*60*1000), (26140, 1, corpsePosition)
 		end
 		if rand == 2 then
 			Game.createItem(26138, 1, corpsePosition):setActionId(5580)
 			addEvent(function()
 				local teleport2 = Tile(corpsePosition):getItemById(26138)
-			teleport2:remove(1) end, (1*60*1000), 26138, 1, corpsePosition)
+			teleport2:remove(1) end, (1*60*1000), (26138, 1, corpsePosition)
 		end
 	end
 	return true
